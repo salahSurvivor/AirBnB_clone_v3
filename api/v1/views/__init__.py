403e7100ab66f485b12049911ec33f6afafb933e
salahSurvivor <salaheddinenaji2000@gmail.com>
2024-02-27 22:01:48 +0000

    update __init__.py file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-""" init is everything """
+'''
+Creates a Blueprint instance with `url_prefix` set to `/api/v1`.
+'''
+
+
 from flask import Blueprint
 
-
-app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
-
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
 from api.v1.views.states import *
@@ -13,3 +15,4 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
