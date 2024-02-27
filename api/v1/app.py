80b91b4328ae28d1442b2fe2c5a379091307f6d5
Salma Fithi <128839637+Semagrb@users.noreply.github.com>
2024-02-27 22:04:05 +0100

    Task 12 has been updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,31 +1,42 @@
 #!/usr/bin/python3
-"""a script to return the status of the API!"""
-from flask import Flask, jsonify, make_response
+'''
+Createw Flask app; and register the blueprint app_views to Flask instance app.
+'''
+
+from os import getenv
+from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-from flask_cors import CORS
-import os
-
 
 app = Flask(__name__)
-CORS(app, origins=['0.0.0.0'])
+
+# enable CORS and allow for origins:
+CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
+
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def session_close(exception):
-    """close session"""
+def teardown_engine(exception):
+    '''
+    Removes the current SQLAlchemy Session object after each request.
+    '''
     storage.close()
 
 
+# Error handlers for expected app behavior:
 @app.errorhandler(404)
-def handle_404(exep):
-    """error handler"""
-    return make_response(jsonify({"error": "Not found"}), 404)
+def not_found(error):
+    '''
+    Return errmsg `Not Found`.
+    '''
+    response = {'error': 'Not found'}
+    return jsonify(response), 404
 
 
-if __name__ == "__main__":
-    """not importing module if it was imported"""
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = os.getenv('HBNB_API_PORT', 5000)
-    app.run(host=host, port=port, threaded=True)
+if __name__ == '__main__':
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(getenv('HBNB_API_PORT', 5000))
+    app.run(host=HOST, port=PORT, threaded=True)
